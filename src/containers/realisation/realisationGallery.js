1bfd29fbf3846c52ace0ac91960f45efae5734d8
Nounbiep-stephane-jordan <92888921+Nounbiep-stephane-jordan@users.noreply.github.com>
2023-06-16 18:33:36 +0100

    updates

--- src/containers/realisation/realisationGallery.js
+++ src/containers/realisation/realisationGallery.js
@@ -12,14 +12,7 @@ const RealisationGallery = ({heading,text,images}) => {
   };
   return (
     <>
-    {/* <h2>{heading}</h2>
-    <div class="realisation gallery">
-        {images.map((img,index)=>(
-               <div>
-               <img alt="" src={img.src} />
-                </div>
-        ))}
-</div> */}
+ 
 <section className="services">
       <h1>
         Realisation <span>{heading}</span>
